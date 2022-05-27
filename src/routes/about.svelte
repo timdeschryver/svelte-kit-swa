fb80553dfc75e27e721fcccf538b4c4d7cdbfe59
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-05-27 19:15:40 +0200

    add SWA

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,10 +8,6 @@
 	// ...but if the client-side router is already loaded
 	// (i.e. we came here from elsewhere in the app), use it
 	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
 </script>
 
 <svelte:head>
@@ -33,11 +29,6 @@
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
 	</p>
 </div>
 
